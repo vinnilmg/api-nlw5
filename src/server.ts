6d0c5c17930b5b4ec3a42851be50d1cb5d87a5ae
vinnilmg <vini.delimagomes@gmail.com>
2021-05-03 23:30:20 -0300

    aluna2

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,11 @@
 import express from 'express';
+import { routes } from './routes';
+
+import './database'; // reconhece o index
 
 const app = express();
 
-//rotas
-app.get("/", (request, response) => {
-    return response.json({
-        version: "1.0.0",
-    });
-});
-
-app.post("/", (request, response) => {
-    return response.json({
-        message: "Usuário salvo",
-    });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
